c8f233182dedfd7d550cfc48c018af33d40e5aff
tealeeseng <tealeeseng@gmail.com>
2019-07-24 15:39:04 +0800

    flask form to python field

--- webapp/server.py
+++ webapp/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,21 @@ def index():
 @app.route('/createResumePage')
 def createResumePage():
     return render_template('createResumePage.html')
+
+@app.route('/createResumeAction', methods=['POST'])
+def createResumeAction():
+    # print(request.form)
+    name = request.form['name']
+    profileURL = request.form['profileURL']
+    role = request.form['role']
+    about = request.form['about']
+    experience = request.form['experience']
+    education = request.form['education']
+    licensesCertifications = request.form['licensesCertifications']
+    skillsEndorsements = request.form['skillsEndorsements']
+
+
+    return render_template('createResumePageResult.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug = True)
